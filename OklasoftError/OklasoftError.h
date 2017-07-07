001a6fb6bb3f90ee691ab34d813c823aa1db1bbd
Justin Oakes <justin@justins-macbook-pro.local>
2017-07-06 20:52:52 -0500

    Initial Commit

---
+++ OklasoftError/OklasoftError.h
@@ -0,0 +1,19 @@
+//
+//  OklasoftError.h
+//  OklasoftError
+//
+//  Created by Justin Oakes on 7/6/17.
+//  Copyright © 2017 Oklasoft LLC. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for OklasoftError.
+FOUNDATION_EXPORT double OklasoftErrorVersionNumber;
+
+//! Project version string for OklasoftError.
+FOUNDATION_EXPORT const unsigned char OklasoftErrorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OklasoftError/PublicHeader.h>
+
+
