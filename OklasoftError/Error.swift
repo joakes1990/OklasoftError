f25166e40306f0051b830920016628262397efa5
Justin Oakes <justin@justins-macbook-pro.local>
2017-07-06 20:55:32 -0500

    extracted error to it's own framework

---
+++ OklasoftError/Error.swift
@@ -0,0 +1,29 @@
+//
+//  Error.swift
+//  OklasoftError
+//
+//  Created by Justin Oakes on 7/6/17.
+//  Copyright © 2017 Oklasoft LLC. All rights reserved.
+//
+
+import Foundation
+
+public struct oklasoftError: Error {
+    let domain: String = "com.oklasoft"
+    var errorCode: Int
+    var userInfo: [AnyHashable : Any]?
+    var localizedDescription: String
+    
+    public init(errorCode: Int, userInfo: [AnyHashable : Any]?, localizedDescription: String) {
+        self.errorCode = errorCode
+        self.userInfo = userInfo
+        self.localizedDescription = localizedDescription
+    }
+    
+    func toError() -> Error {
+        let newError: NSError = NSError(domain: domain,
+                                        code: errorCode,
+                                        userInfo: userInfo)
+        return newError as Error
+    }
+}
