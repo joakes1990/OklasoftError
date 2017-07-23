b0e2eb7f0c00a96cf2267e30d1d58ba5947e113e
Justin Oakes <justin@justins-macbook-pro.local>
2017-07-22 19:32:41 -0500

    Added iOS target to Oklasoft Error

---
+++ OklasoftError(iOS)/OklasoftError_iOS_.h
@@ -0,0 +1,19 @@
+//
+//  OklasoftError_iOS_.h
+//  OklasoftError(iOS)
+//
+//  Created by Justin Oakes on 7/22/17.
+//  Copyright © 2017 Oklasoft LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OklasoftError_iOS_.
+FOUNDATION_EXPORT double OklasoftError_iOS_VersionNumber;
+
+//! Project version string for OklasoftError_iOS_.
+FOUNDATION_EXPORT const unsigned char OklasoftError_iOS_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <OklasoftError_iOS_/PublicHeader.h>
+
+
